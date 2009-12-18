3fe40b1ff2dc2a9fabce61033a0a3797ba263bed
linzhenqun <linzhenqun@785468e8-de65-11dd-a893-8d7ce9fbef62>
2009-12-18 19:06:13 +0000

    窗口类代码小调整

--- KamaLib/Projects/ViewDemo/ViewDemo.cpp
+++ KamaLib/Projects/ViewDemo/ViewDemo.cpp
@@ -6,13 +6,31 @@
 #include "KmCommons.h"
 using namespace kama;
 
+class KTestFrame: public KMainFrame
+{
+protected:
+	virtual void DoPaint(HDC dc)
+	{
+		KMainFrame::DoPaint(dc);
+	}
+
+	virtual BOOL WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam, LRESULT& ret)
+	{
+		KTestFrame::WndProc(hwnd, msg, wparam, lparam, ret);
+	}
+};
+
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow)
 {
-	KMainFrame mainFrame;
-	mainFrame.Create();
+	KTestFrame mainFrame;
+	mainFrame.Create(0, 0, 800, 600, 
+		biSysMenu | biMinimize | biMaximize, 
+		bsSizeable, 
+		L"第一个窗口", 
+		LoadIcon(NULL, IDI_APPLICATION));
 	mainFrame.AlignWindow();
 	mainFrame.Show();
 
