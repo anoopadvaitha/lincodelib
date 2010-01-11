0ee64869e639c5dd28314f1070b42c8a3275fb87
linzhenqun <linzhenqun@785468e8-de65-11dd-a893-8d7ce9fbef62>
2010-01-11 16:29:34 +0000

    增加视图控件工程，现实按钮类

---
+++ trunk/KamaLib/Projects/CtrlDemo/CtrlDemo.cpp
@@ -0,0 +1,92 @@
+// CtrlDemo.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "CtrlDemo.h"
+#include "KmDxViews.h"
+#include "KmDxRender.h"
+#include "KmDxCtrls.h"
+using namespace kama;
+
+class KTestApp: public KDxApp
+{
+public:
+	virtual void Initialize()
+	{
+		mRunAlways = TRUE;
+
+		// 主窗口
+		mMainFrame.Create();
+		mMainFrame.AlignWindow();
+		mMainFrame.Show();
+
+		// 渲染器
+		mRender.SetSize(mMainFrame.Width(), mMainFrame.Height());
+		mRender.SetWindow(mMainFrame.Handle());
+		mRender.SetBkColor(D3DCOLOR_ARGB(255, 198, 222, 242));
+		//Render.SetVerticalSync(FALSE);
+		mRender.Initialize();
+
+		// 视图屏幕
+		mScreen = NEW_SCREEN(KDxScreen);
+		mScreen->SetHostWnd(mMainFrame.Handle());
+		mScreen->SetRender(&mRender);
+
+		// 窗口
+		KDxWindow* wnd = NEW_WINDOW(KDxWindow, mScreen);
+		wnd->SetPos(10, 10);
+		wnd->SetSize(300, 300);
+		wnd->Show();
+
+		// 按钮
+		KDxButton* btn = NEW_CONTROL(KDxButton, wnd, mScreen);
+		btn->SetCaption(L"我是按钮");
+		btn->SetPos(10, 10);
+
+		KDxApp::Initialize();
+	}
+	virtual void Finalize()
+	{
+		// 视图屏幕
+		FREE_SCREEN(mScreen);
+		mScreen = NULL;
+
+		// 渲染器
+		mRender.Finalize();
+
+		KDxApp::Finalize();
+	}
+protected:
+	virtual void UpdateFrame()
+	{
+		mScreen->Update();
+	}
+	virtual void RenderFrame()
+	{
+		if (mRender.BeginPaint())
+		{
+			mScreen->Paint();
+		}
+		mRender.EndPaint();
+	}
+	virtual KDxMainFrame* MainFrame()
+	{
+		return &mMainFrame;
+	}
+private:
+	KDxMainFrame	mMainFrame;
+	KDxRender		mRender;
+	KDxScreen*		mScreen;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	KTestApp app;
+	app.Initialize();
+	app.Run();
+	app.Finalize();
+	return 0;
+}
